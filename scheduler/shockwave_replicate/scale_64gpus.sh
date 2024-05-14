195b6fcaeeafd4862f3f32dc0bcfeb6c0553b0ff
JitongZ <justinzhou650@gmail.com>
2024-05-14 01:32:26 +0000

    add scripts to run simulation and create plots

---
+++ scheduler/shockwave_replicate/scale_64gpus.sh
@@ -0,0 +1,19 @@
+# python3 ../scripts/drivers/simulate_scheduler_with_trace.py \
+#   --trace_file ../traces/shockwave/220_0.2_5_100_25_4_0,0.5,0.5_0.6,0.3,0.09,0.01_multigpu_dynamic.trace \
+#   --policy "max_min_fairness" \
+#   --throughputs_file ../shockwave_wisr_throughputs.json \
+#   --cluster_spec 64:0:0 \
+#   --seed 0 --solver ECOS \
+#   --time_per_iteration 120 \
+#   # --config ../scale_64gpus.json \
+#   # --pickle_output_dir ./results/pickles \
+#   > ./results/test_max_min_fairness.txt 2>&1
+
+python3 ../scripts/drivers/simulate_scheduler_with_trace.py \
+  --trace_file ../traces/physical_cluster/artifact_evaluation.trace \
+  --policy "max_min_fairness" \
+  --throughputs_file ../simulation_throughputs.json \
+  --cluster_spec 64:0:0 \
+  --seed 0 --solver ECOS \
+  --time_per_iteration 120 \
+  > ./results/test_max_min_fairness.txt 2>&1
